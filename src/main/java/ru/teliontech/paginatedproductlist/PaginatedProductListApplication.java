e0731760401a70165ad19d8cee32397700d80e00
Tarasov Sergei <www.neon@mail.ru>
2023-06-27 11:55:02 +0500

    initial commit

---
+++ src/main/java/ru/teliontech/paginatedproductlist/PaginatedProductListApplication.java
@@ -0,0 +1,13 @@
+package ru.teliontech.paginatedproductlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginatedProductListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginatedProductListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/teliontech/paginatedproductlist/PaginatedProductListApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.teliontech.paginatedproductlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginatedProductListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
